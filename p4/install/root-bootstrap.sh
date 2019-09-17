9f1e0fda1c74987b2ee9accf0a4cd716f739fa8f
suresh <sureshkumarr.s@gmail.com>
2019-09-17 11:34:08 +0530

    p4 updated

---
+++ p4/install/root-bootstrap.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# Print commands and exit on errors
+set -xe
+
+
+apt-get update
+
+apt-get install -y --no-install-recommends --fix-missing\
+  autoconf \
+  automake \
+  bison \
+  build-essential \
+  ca-certificates \
+  cmake \
+  cpp \
+  curl \
+  emacs24 \
+  flex \
+  git \
+  libboost-dev \
+  libboost-filesystem-dev \
+  libboost-iostreams1.58-dev \
+  libboost-program-options-dev \
+  libboost-system-dev \
+  libboost-test-dev \
+  libboost-thread-dev \
+  libc6-dev \
+  libevent-dev \
+  libffi-dev \
+  libfl-dev \
+  libgc-dev \
+  libgc1c2 \
+  libgflags-dev \
+  libgmp-dev \
+  libgmp10 \
+  libgmpxx4ldbl \
+  libjudy-dev \
+  libpcap-dev \
+  libreadline6 \
+  libreadline6-dev \
+  libssl-dev \
+  libtool \
+  linux-headers-$KERNEL\
+  lubuntu-desktop \
+  make \
+  mktemp \
+  pkg-config \
+  python \
+  python-dev \
+  python-ipaddr \
+  python-pip \
+  python-psutil \
+  python-scapy \
+  python-setuptools \
+  tcpdump \
+  unzip \
+  vim \
+  wget \
+  xcscope-el \
+  xterm
+
+useradd -m -d /home/p4 -s /bin/bash p4
+echo "p4:p4" | chpasswd
+echo "p4 ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/99_p4
+chmod 440 /etc/sudoers.d/99_p4
+usermod -aG vboxsf p4
+
