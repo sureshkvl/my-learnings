268c28a923ec755e9a33a9544bcd42c8474b11c2
Default user for cloud <cloud@node1.cluster.local>
2019-10-16 10:52:56 +0000

    updaed efk

--- kubernetes-examples/efk-logging/deploy.sh
+++ kubernetes-examples/efk-logging/deploy.sh
@@ -1,6 +1,6 @@
 kubectl create namespace logging
 kubectl create -f elastic.yaml -n logging
 kubectl create -f kibana.yaml -n logging
-kubectl creaet -f fluentd-daemonset-elasticsearch-rbac
+kubectl create -f fluentd-daemonset-elasticsearch-rbac.yaml
 kubectl get pods -n logging
 kubectl get svc -n logging
